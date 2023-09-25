896c2fad44ab0b0d00b1351a3f18614e3a53d631
Gabriel Rocha <rochafrgabriel@gmail.com>
2023-09-25 20:54:14 -0300

    insertion sort

---
+++ 07-class/insertion_sort.cpp
@@ -0,0 +1,35 @@
+#include <stdio.h>
+
+int insertion_sort_with_temp(int *arr, int n) {
+  int temp, iter = 0;
+
+  for (int i = 1; i < n; i++) {
+    temp = arr[i];
+    int j = i - 1;
+
+    while (j >= 0 && arr[j] > temp) {
+      arr[j + 1] = arr[j];
+      j--;
+      iter++;
+    }
+
+    arr[j + 1] = temp;
+  }
+
+  return iter;
+}
+
+int main () {
+  int arr[] = {2, 7, 4, 15, 9};
+  int size = sizeof(arr) / sizeof(arr[0]);
+
+  int swap = insertion_sort_with_temp(arr, size);
+
+  for (int i = 0; i < size; i++) {
+    printf("%d ", arr[i]);
+  }
+
+  printf("swapped %i times\n", swap);
+
+  return 0;
+}
