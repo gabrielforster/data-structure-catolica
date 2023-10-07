dc53291824c6928e53978e8654bc71a158c1f401
Gabriel Rocha <rochafrgabriel@gmail.com>
2023-10-07 02:29:49 -0300

    making heap_sort function receive vector size as arg

--- n2-assignment/heap_sort.cpp
+++ n2-assignment/heap_sort.cpp
@@ -5,8 +5,7 @@
 Nomes: Gabriel Forster Rocha, Pablo Arman Schuller, Endric Mateus Fruhauf
 
 Conseguiu chegar no resultado?
-
-Se nao, por que nao conseguiu chegar no resultado?
+- Sim
 
 */
 #include <stdlib.h>
@@ -16,11 +15,66 @@ Se nao, por que nao conseguiu chegar no resultado?
 #define TAMANHO_VETOR 10000
 #define RANDOM 100000
 
-// Prototipacao das funcoes
-void heapify(int *vetor, int tamanho, int n, int *qtd_comparacoes, int *qtd_trocas);
-void heap_sort(int *vetor);
+// Prototipando as funcoes
 void gera_valores_vetor(int *vetor, int op);
-void imprimir_vetor(int *vetor);
+void imprimir_vetor(int *vetor, int tamanho);
+void heap_sort(int *vetor, int tamanho);
+void heapify(int *vetor, int tamanho, int n, int *qtd_comparacoes, int *qtd_trocas);
+
+int main (void) {
+  int vetor[TAMANHO_VETOR];
+  int opcao, i;
+
+  printf("Escolha uma opcao:\n");
+  printf("1 - Melhor caso\n");
+  printf("2 - Pior caso\n");
+  printf("3 - Caso medio\n");
+  printf("Opcao: ");
+  scanf("%i", &opcao);
+
+  printf("opcao escolhida: %i\n", opcao);
+
+  gera_valores_vetor(vetor, opcao);
+
+  printf("Vetor gerado: ");
+  imprimir_vetor(vetor, TAMANHO_VETOR);
+
+  heap_sort(vetor, TAMANHO_VETOR);
+
+  printf("Vetor ordenado: ");
+  imprimir_vetor(vetor, TAMANHO_VETOR);
+
+  return 0;
+}
+
+void gera_valores_vetor(int *vetor, int op) {
+  int i, j;
+  switch (op) {
+    case 1: // Gera valores ordenados para o vetor (melhor caso)
+      for (i = 0; i < TAMANHO_VETOR; i++) {
+        vetor[i] = i;
+      }
+      break;
+    case 2: // gera valores ordenados de forma invertida (pior caso)
+      for (i = TAMANHO_VETOR, j = 0; i > 0; i--, j++) {
+        vetor[j] = i;
+      }
+      break;
+    case 3: // gera valores de forma aleatoria para o vetor (caso medio)
+      for (i = 0; i < TAMANHO_VETOR; i++) {
+        vetor[i] = rand() % RANDOM;
+      }
+      break;
+  }
+}
+
+void imprimir_vetor(int *vetor, int tamanho) {
+  int i;
+  for (i = 0; i < tamanho; i++) {
+    printf("%d ", vetor[i]);
+  }
+  printf("\n");
+}
 
 void heapify(int *vetor, int tamanho, int n, int *qtd_comparacoes, int *qtd_trocas) {
   int maior = n; // Inicializa o maior como raiz
@@ -54,24 +108,24 @@ void heapify(int *vetor, int tamanho, int n, int *qtd_comparacoes, int *qtd_troc
 }
 
 
-void heap_sort (int *vetor) {
+void heap_sort (int *vetor, int tamanho) {
   int i, aux, qtd_comparacoes = 0, qtd_trocas = 0;
   clock_t tempo_inicio = clock();
 
-  // Constroi o heap (rearranja o array)
-  for (i = TAMANHO_VETOR / 2 - 1; i >= 0; i--) {
-    heapify(vetor, TAMANHO_VETOR, i, &qtd_comparacoes, &qtd_trocas);
+  // Constroi o heap apartir de cada elemento do vetor (rearranja o vetor)
+  for (i = tamanho / 2 - 1; i >= 0; i--) {
+    heapify(vetor, tamanho, i, &qtd_comparacoes, &qtd_trocas);
   }
 
-  // Extrai elementos do heap um por um
-  for (i = TAMANHO_VETOR - 1; i > 0; i--) {
-    // Move a raiz atual para o final
+  // Extrai, um por um, todos os elementos do heap 
+  for (i = tamanho - 1; i > 0; i--) {
+    // Move a raiz atual para o final do vetor
     aux = vetor[0];
     vetor[0] = vetor[i];
     vetor[i] = aux;
     qtd_trocas++;
 
-    // Chama o heapify na subárvore reduzida
+    // Chama o heapify na subarvore reduzida
     heapify(vetor, i, 0, &qtd_comparacoes, &qtd_trocas);
   }
 
@@ -82,55 +136,4 @@ void heap_sort (int *vetor) {
   printf("\nQuantidade de comparacoes: %i\n", qtd_comparacoes);
   printf("Quantidade de trocas: %i\n", qtd_trocas);
   printf("Tempo de execucao do algoritmo: %.3f \n\n",tempo_final/1000);
-}
-
-void gera_valores_vetor(int *vetor, int op) {
-  int i, j;
-  switch (op) {
-    case 1: // Gera valores ordenados para o vetor (melhor caso)
-      for (i = 0; i < TAMANHO_VETOR; i++) {
-        vetor[i] = i;
-      }
-      break;
-    case 2: // gera valores ordenados de forma invertida (pior caso)
-      for (i = TAMANHO_VETOR, j = 0; i > 0; i--, j++) {
-        vetor[j] = i;
-      }
-      break;
-    case 3: // gera valores de forma aleatoria para o vetor (caso medio)
-      for (i = 0; i < TAMANHO_VETOR; i++) {
-        vetor[i] = rand() % RANDOM;
-      }
-      break;
-  }
-}
-
-void imprimir_vetor(int *vetor) {
-  int i;
-  for (i = 0; i < TAMANHO_VETOR; i++) {
-    printf("%d ", vetor[i]);
-  }
-  printf("\n");
-}
-
-int main (void) {
-  int vetor[TAMANHO_VETOR];
-  int opcao, i;
-  double tempo_gasto;
-
-
-  printf("Escolha uma opcao:\n");
-  printf("1 - Melhor caso\n");
-  printf("2 - Pior caso\n");
-  printf("3 - Caso medio\n");
-  printf("Opcao: ");
-  scanf("%i", &opcao);
-
-  printf("opcao escolhida: %i\n", opcao);
-
-  gera_valores_vetor(vetor, opcao);
-
-  heap_sort(vetor);
-
-  return 0;
 }
